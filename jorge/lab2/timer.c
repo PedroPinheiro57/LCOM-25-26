4a2dc11c5b8d1e1bddf40b4085f6b6659cf3383f
JorgeCunha06 <up202405044@g.uporto.pt>
2026-03-24 15:53:08 +0000

    Lab2 - Reading and Programming the Timers

--- jorge/lab2/timer.c
+++ jorge/lab2/timer.c
@@ -6,10 +6,26 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (timer > 2 || freq < 19 || freq > TIMER_FREQ) return 1;
 
-  return 1;
+  uint8_t st;
+  if (timer_get_conf(timer, &st) != 0) return 1;
+
+  uint8_t control_word = (st & 0x0F);
+  
+  control_word |= (timer << 6) | TIMER_LSB_MSB;
+
+  uint16_t divisor = (uint16_t) (TIMER_FREQ / freq);
+  uint8_t lsb, msb;
+  util_get_LSB(divisor, &lsb);
+  util_get_MSB(divisor, &msb);
+
+  if (sys_outb(TIMER_CTRL, control_word) != 0) return 1;
+
+  if (sys_outb(TIMER_0 + timer, lsb) != 0) return 1;
+  if (sys_outb(TIMER_0 + timer, msb) != 0) return 1;
+
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -32,16 +48,35 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (st == NULL || timer > 2) return 1;
 
-  return 1;
+  uint8_t rb_cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+
+  if (sys_outb(TIMER_CTRL, rb_cmd) != 0) return 1;
+  
+  return util_sys_inb(TIMER_0 + timer, st);
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+  union timer_status_field_val val;
 
-  return 1;
+  switch (field) {
+    case tsf_all:
+      val.byte = st;
+      break;
+    case tsf_initial:
+      val.in_mode = (st >> 4) & 0x03;
+      break;
+    case tsf_mode:
+      val.count_mode = (st >> 1) & 0x07;
+      if (val.count_mode == 6) val.count_mode = 2; 
+      if (val.count_mode == 7) val.count_mode = 3;
+      break;
+    case tsf_base:
+      val.bcd = st & TIMER_BCD;
+      break;
+    default: return 1;
+  }
+
+  return timer_print_config(timer, field, val);
 }

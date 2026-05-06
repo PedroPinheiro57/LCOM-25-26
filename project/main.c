cf1cd73fcb87cf3984d3a55d977ccc99fe61ec92
Pedro Pinheiro <pedroppinheiro2@gmail.com>
2026-05-06 11:58:43 +0100

    added some keyboard utils

--- project/main.c
+++ project/main.c
@@ -1,24 +1,60 @@
 #include <lcom/lcf.h>
 #include "../pedro/lab5/video.h"
+#include "../pedro/lab3/kbc.h"
+#include <lcom/timer.h>
+#include "devices/timer.h"
+#include "devices/keyboard.h"
 
-int main(int argc, char *argv[]) {
-  lcf_set_language("EN-US");
-  lcf_trace_calls("/home/lcom/labs/project/trace.txt");
-  lcf_log_output("/home/lcom/labs/project/output.txt");
-  if (lcf_start(argc, argv))
+
+extern int foo(void);
+int(proj_main_loop)(int argc, char *argv[]) {
+  foo();
+  uint8_t kbd_bit, timer_bit;
+
+  if (timer_subscribe_int(&timer_bit) != 0) return 1;
+  if (kbc_subscribe_int(&kbd_bit) != 0) {
+    timer_unsubscribe_int();
     return 1;
-  lcf_cleanup();
+  }
+
+  bool done = false;
+  int r, ipc_status;
+  message msg;
+
+  while (!done) {
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) continue;
+    if (!is_ipc_notify(ipc_status)) continue;
+    if (_ENDPOINT_P(msg.m_source) != HARDWARE) continue;
+
+    uint32_t irqs = msg.m_notify.interrupts;
+
+    if (irqs & BIT(timer_bit)) {
+      timer_int_handler();
+    }
+
+    if (irqs & BIT(kbd_bit)) {
+      kbc_ih();
+      if (!kbc_has_error()) {
+        uint8_t sc = kbc_get_scancode_byte();
+        if (key_is_make(sc))
+          printf("key: 0x%02X\n", key_get_code(sc));
+        if (key_get_code(sc) == KEY_ESC && !key_is_make(sc))
+          done = true;
+      }
+    }
+  }
+
+  kbc_unsubscribe_int();
+  timer_unsubscribe_int();
   return 0;
 }
 
-int(proj_main_loop)(int argc, char *argv[]) {
-  if (video_map_vram(0x115) != 0) return 1;
-  if (video_set_mode(0x115) != 0) return 1;
-
-  video_clear_screen(0x1a1a2e);
-
-  tickdelay(micros_to_ticks(3000000u));
-
-  vg_exit();
+int main(int argc, char *argv[]) {
+  lcf_set_language("EN-US");
+  lcf_trace_calls("/home/lcom/labs/grupo_2leic01_5/project/trace.txt");
+  lcf_log_output("/home/lcom/labs/grupo_2leic01_5/project/output.txt");
+  if (lcf_start(argc, argv))
+    return 1;
+  lcf_cleanup();
   return 0;
 }
